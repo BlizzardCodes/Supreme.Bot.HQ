31b293a8a63b929511f0acb765df24be3620372b
Caleb <calebteo24@gmail.com>
2021-03-08 09:30:29 -0800

    commiting some code for S.B.HQ

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('ODE4NTE3ODA5MTc5OTE4Mzg4.YEZOHQ.ReUsi3L3GS2cVyse8F0_HsYmBcg');
