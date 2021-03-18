ed830050d7cee7ddab554df3ed732432877f88e8
Caleb <calebteo24@gmail.com>
2021-03-18 15:43:05 -0700

    added my first follow up question.

--- commands/start.js
+++ commands/start.js
@@ -4,20 +4,20 @@ module.exports = {
 
     execute(message) {
         message.author.send(`Why hello there ${message.author.username},`)
-        const tasks = require('./tasks.json');
-        const item = tasks
-        const filter = response => {
-            return item.answer1.some(awnser => awnser.toLowerCase() === response.content.toLowerCase());
-        };
+        const quiz = require('./tasks.json');
+const item = quiz[Math.floor(Math.random() * quiz.length)];
+const filter = response => {
+	return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
 
-        message.author.send(item.question).then(() => {
-            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-                .then(collected => {
-                    message.channel.send(`${collected.first().author} got the correct answer!`);
-                })
-                .catch(collected => {
-                    message.channel.send('Looks like nobody got the answer this time.');
-                });
-        });
+message.author.send(item.question).then(() => {
+	message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+		.then(collected => {
+			message.channel.send(`${collected.first().author} Great, I have noted that down.`);
+		})
+		.catch(collected => {
+			message.channel.send('It looks like you did not awnser in time, please do "^start" to try again.');
+		});
+});
     }
 }
